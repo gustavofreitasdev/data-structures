152a95f50476438b45f7ab1dadf2bc7bd4926887
Gustavo F. Silva <gustavofsilva.dev@gmail.com>
2018-04-22 22:15:50 -0300

    Adicionando documentários a implementação do Grafo de Matriz de Adjacencia

--- graphs/grafo_matriz_adjacencia.c
+++ graphs/grafo_matriz_adjacencia.c
@@ -2,13 +2,15 @@
 
 unsigned int **GrafoMatrizAdjacencia_alocaMatrizAdjacentes(unsigned int vertices, unsigned int valor){
     unsigned int aux_vertices, aux2_vertices;
-    unsigned int **matriz_adjacentes = malloc(vertices * sizeof(unsigned int *));
-    for(aux_vertices=0; aux_vertices<vertices; aux_vertices++){
-        matriz_adjacentes[aux_vertices] = malloc(vertices * sizeof(unsigned int));
+    unsigned int **matriz_adjacentes = malloc(vertices * sizeof(unsigned int *)); // aloca as linhas da matriz de adjacencia
+    for(aux_vertices=0; aux_vertices<vertices; aux_vertices++){ 
+        // loop por todas as linhas da matriz adjacentes
+        matriz_adjacentes[aux_vertices] = malloc(vertices * sizeof(unsigned int)); // aloca todas as colunas da matriz de adjacencia
     }
+    // loop por toda a matriz de adjacencia
     for(aux_vertices=0; aux_vertices<vertices; aux_vertices++){
         for(aux2_vertices=0; aux2_vertices<vertices; aux2_vertices++){
-            matriz_adjacentes[aux_vertices][aux2_vertices] = valor;
+            matriz_adjacentes[aux_vertices][aux2_vertices] = valor; // inicia todos os valores das aretas com o valor passado
         }
     }
 
@@ -25,6 +27,7 @@ GrafoMatrizAdjacencia *GrafoMatrizAdjacencia_criar(unsigned int vertices){
 
 void GrafoMatrizAdjacencia_insereAresta(GrafoMatrizAdjacencia *grafo, unsigned int vertice1, unsigned int vertice2){
     if(vertice1 != vertice2 && grafo->matriz_adjacentes[vertice1][vertice2] == 0){
+        // caso os vertices passsados não sejem iguais, e a aresta não existir
         grafo->matriz_adjacentes[vertice1][vertice2] = 1;
         grafo->matriz_adjacentes[vertice2][vertice1] = 1;
         grafo->arestas++;
@@ -33,6 +36,7 @@ void GrafoMatrizAdjacencia_insereAresta(GrafoMatrizAdjacencia *grafo, unsigned i
 
 void GrafoMatrizAdjacencia_removeAresta(GrafoMatrizAdjacencia *grafo, unsigned int vertice1, unsigned int vertice2){
     if(vertice1 != vertice2 && grafo->matriz_adjacentes[vertice1][vertice2] == 1){
+        // caso os vertices passsados não sejem iguais, e a aresta existir
         grafo->matriz_adjacentes[vertice1][vertice2] = 0;
         grafo->matriz_adjacentes[vertice2][vertice1] = 0;      
         grafo->arestas--;  
@@ -41,7 +45,7 @@ void GrafoMatrizAdjacencia_removeAresta(GrafoMatrizAdjacencia *grafo, unsigned i
 
 char GrafoMatrizAdjacencia_adjacentes(GrafoMatrizAdjacencia *grafo, unsigned int vertice1, unsigned int vertice2){
     char adjacentes = 1;
-    if(vertice1 != vertice2 && grafo->matriz_adjacentes[vertice1][vertice2] == 0)
+    if(vertice1 != vertice2 && grafo->matriz_adjacentes[vertice1][vertice2] == 0) // caso os vertices passsados não sejem iguais, e a aresta não existir
         adjacentes = 0;
 
     return adjacentes;
@@ -50,7 +54,8 @@ char GrafoMatrizAdjacencia_adjacentes(GrafoMatrizAdjacencia *grafo, unsigned int
 unsigned int GrafoMatrizAdjacencia_grauVertice(GrafoMatrizAdjacencia *grafo, unsigned int vertice){
     unsigned int grau = 0, cursor;
     for(cursor=0; cursor<grafo->vertices; cursor++){
-        if(grafo->matriz_adjacentes[vertice][cursor] == 1)
+        // loop por todas as arestas do vertice passado
+        if(grafo->matriz_adjacentes[vertice][cursor] == 1) // caso exista uma aresta 
             grau++;
     }
 
